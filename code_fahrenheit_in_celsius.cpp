429985671a0c536d9f369a8f37f7b2324eff4399
Siripong Franzen <siripongfranzen@me.com>
2011-10-23 18:06:14 +0700

    Fahrenheit in Celsius Converter added!

---
+++ code_fahrenheit_in_celsius.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double fahrenheit;
+	double celsius;
+	
+	cout << " Please enter the Temperature in Fahrenheit: ";
+	cin >> fahrenheit;
+	
+	celsius = (fahrenheit - 32) * 5.0 / 9.0;
+	
+	cout << " " << fahrenheit;
+	cout << " Degrees Fahrenheit corresponds ";
+	cout << celsius << " Degrees Celsius" << endl;
+		
+	return 0;
+	
+}
